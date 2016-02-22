a48f278744928b89f6cb407e99af78332b3c57b2
Diego Aguado <diego@insydo.com>
2016-02-22 18:01:38 +0400

    more models from theano tutorial

--- FirstNN_w_CIFAR.py
+++ FirstNN_w_CIFAR.py
@@ -1,4 +1,15 @@
-__author__ = 'diegoinsydo'
+from __future__ import print_function
+import os
+import sys
+import timeit
+
+import numpy
+
+import theano
+import theano.tensor as T
+
+
+from LogisticRegression import LogisticRegression, load_data
 import numpy as np
 import theano
 import theano.tensor as T
@@ -23,48 +34,216 @@ Yte = np.asarray(test["labels"])
 
 x = T.dvector()
 y = T.dscalar()
-NN=False
+NN = True
 
-if NN :
+if NN:
 
-    def layer(x, w):
-        b = np.array([1], dtype=theano.config.floatX)
-        new_x = T.concatenate([x, b])
-        m = T.dot(w.T, new_x)
-        h = nnet.sigmoid(m)
-        return h
+    # start-snippet-1
+    class HiddenLayer(object):
+        def __init__(self, rng, input, n_in, n_out, W=None, b=None,
+                     activation=T.tanh):
+            """
+            Typical hidden layer of a MLP: units are fully-connected and have
+            sigmoidal activation function. Weight matrix W is of shape (n_in,n_out)
+            and the bias vector b is of shape (n_out,).
 
-    def grad_desc(cost, theta):
-        alpha = 0.1 #learning rate
-        return theta - (alpha * T.grad(cost, wrt=theta))
+            NOTE : The nonlinearity used here is tanh
 
-    theta1 = theano.shared(np.array(np.random.rand(3073,3), dtype=theano.config.floatX))
-    theta2 = theano.shared(np.array(np.random.rand(4,1), dtype=theano.config.floatX))
+            Hidden unit activation is given by: tanh(dot(input,W) + b)
+
+            :type rng: numpy.random.RandomState
+            :param rng: a random number generator used to initialize weights
+
+            :type input: theano.tensor.dmatrix
+            :param input: a symbolic tensor of shape (n_examples, n_in)
+
+            :type n_in: int
+            :param n_in: dimensionality of input
+
+            :type n_out: int
+            :param n_out: number of hidden units
+
+            :type activation: theano.Op or function
+            :param activation: Non linearity to be applied in the hidden
+                               layer
+            """
+            self.input = input
+            # end-snippet-1
+
+            # `W` is initialized with `W_values` which is uniformely sampled
+            # from sqrt(-6./(n_in+n_hidden)) and sqrt(6./(n_in+n_hidden))
+            # for tanh activation function
+            # the output of uniform if converted using asarray to dtype
+            # theano.config.floatX so that the code is runable on GPU
+            # Note : optimal initialization of weights is dependent on the
+            #        activation function used (among other things).
+            #        For example, results presented in [Xavier10] suggest that you
+            #        should use 4 times larger initial weights for sigmoid
+            #        compared to tanh
+            #        We have no info for other function, so we use the same as
+            #        tanh.
+            if W is None:
+                W_values = numpy.asarray(
+                    rng.uniform(
+                        low=-numpy.sqrt(6. / (n_in + n_out)),
+                        high=numpy.sqrt(6. / (n_in + n_out)),
+                        size=(n_in, n_out)
+                    ),
+                    dtype=theano.config.floatX
+                )
+                if activation == theano.tensor.nnet.sigmoid:
+                    W_values *= 4
+
+                W = theano.shared(value=W_values, name='W', borrow=True)
+
+            if b is None:
+                b_values = numpy.zeros((n_out,), dtype=theano.config.floatX)
+                b = theano.shared(value=b_values, name='b', borrow=True)
+
+            self.W = W
+            self.b = b
+
+            lin_output = T.dot(input, self.W) + self.b
+            self.output = (
+                lin_output if activation is None
+                else activation(lin_output)
+            )
+            # parameters of the model
+            self.params = [self.W, self.b]
 
 
-    hid1 = layer(x, theta1)
+    # start-snippet-2
+    class MLP(object):
+        """Multi-Layer Perceptron Class
 
-    out1 = T.sum(layer(hid1, theta2))
+        A multilayer perceptron is a feedforward artificial neural network model
+        that has one layer or more of hidden units and nonlinear activations.
+        Intermediate layers usually have as activation function tanh or the
+        sigmoid function (defined here by a ``HiddenLayer`` class)  while the
+        top layer is a softmax layer (defined here by a ``LogisticRegression``
+        class).
+        """
+
+        def __init__(self, rng, input, n_in, n_hidden, n_out):
+            """Initialize the parameters for the multilayer perceptron
+            :type rng: numpy.random.RandomState
+            :param rng: a random number generator used to initialize weights
+
+            :type input: theano.tensor.TensorType
+            :param input: symbolic variable that describes the input of the
+            architecture (one minibatch)
+
+            :type n_in: int
+            :param n_in: number of input units, the dimension of the space in
+            which the datapoints lie
+
+            :type n_hidden: int
+            :param n_hidden: number of hidden units
+
+            :type n_out: int
+            :param n_out: number of output units, the dimension of the space in
+            which the labels lie
+
+            """
+
+            # Since we are dealing with a one hidden layer MLP, this will translate
+            # into a HiddenLayer with a tanh activation function connected to the
+            # LogisticRegression layer; the activation function can be replaced by
+            # sigmoid or any other nonlinear function
+            self.hiddenLayer = HiddenLayer(
+                rng=rng,
+                input=input,
+                n_in=n_in,
+                n_out=n_hidden,
+                activation=T.tanh
+            )
+
+            # The logistic regression layer gets as input the hidden units
+            # of the hidden layer
+            self.logRegressionLayer = LogisticRegression(
+                input=self.hiddenLayer.output,
+                n_in=n_hidden,
+                n_out=n_out
+            )
+            # end-snippet-2 start-snippet-3
+            # L1 norm ; one regularization option is to enforce L1 norm to
+            # be small
+            self.L1 = (
+                abs(self.hiddenLayer.W).sum()
+                + abs(self.logRegressionLayer.W).sum()
+            )
+
+            # square of L2 norm ; one regularization option is to enforce
+            # square of L2 norm to be small
+            self.L2_sqr = (
+                (self.hiddenLayer.W ** 2).sum()
+                + (self.logRegressionLayer.W ** 2).sum()
+            )
+
+            # negative log likelihood of the MLP is given by the negative
+            # log likelihood of the output of the model, computed in the
+            # logistic regression layer
+            self.negative_log_likelihood = (
+                self.logRegressionLayer.negative_log_likelihood
+            )
+            # same holds for the function computing the number of errors
+            self.errors = self.logRegressionLayer.errors
+
+            # the parameters of the model are the parameters of the two layer it is
+            # made out of
+            self.params = self.hiddenLayer.params + self.logRegressionLayer.params
+            # end-snippet-3
+
+            # keep track of model input
+            self.input = input
 
 
-    xent = -y * T.log(out1) - (1-y) * T.log(1-out1) # Cross-entropy loss function
-    xent_f = xent.mean() + 0.01 * (theta2 ** 2).sum()
+    index = T.lscalar()  # index to a [mini]batch
+    x = T.matrix('x')  # the data is presented as rasterized images
+    y = T.ivector('y')  # the labels are presented as 1D vector of
+                        # [int] labels
 
-    cost = theano.function(inputs=[x, y], outputs=xent_f, updates=[
-            (theta1, grad_desc(xent_f, theta1)),
-            (theta2, grad_desc(xent_f, theta2))])
+    rng = numpy.random.RandomState(1234)
 
-    run_forward = theano.function(inputs=[x], outputs=out1)
+    # construct the MLP class
+    classifier = MLP(
+        rng=rng,
+        input=x,
+        n_in=Xtr_rows.shape[0],
+        n_hidden=4,
+        n_out=10
+    )
 
-    for i in range(100):
-        for k in range(len(Xtr_rows)):
-            cur_cost = cost(Xtr_rows[k], Ytr_6[k]) #call our Theano-compiled cost function, it will auto update weights
-            print('Cost: %s' % (cur_cost,))
+    # start-snippet-4
+    # the cost we minimize during training is the negative log likelihood of
+    # the model plus the regularization terms (L1 and L2); cost is expressed
+    # here symbolically
+    L1_reg = 0.0
+    L2_reg = 0.0
+    cost = (
+        classifier.negative_log_likelihood(y)
+        + L1_reg * classifier.L1
+        + L2_reg * classifier.L2_sqr
+    )
+    # end-snippet-4
 
-    for i in range(Xtr_rows.shape[0]):
-        print "prob: ", run_forward(Xtr_rows[i,:])," real ", Ytr[i]
+    # compiling a Theano function that computes the mistakes that are made
+    # by the model on a minibatch
+    test_model = theano.function(
+        inputs=[index],
+        outputs=classifier.errors(y),
+        givens={
+            x: test_set_x[index * batch_size:(index + 1) * batch_size],
+            y: test_set_y[index * batch_size:(index + 1) * batch_size]
+        }
+    )
 
-    print "end"
+
+    MLP( Xtr_rows, Xtr_rows.shape[0], 3, 2)
+
+
+
+
 else:
 
     rng = np.random
